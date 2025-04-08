95ee9bbd08e3ef769ef49bdd3e22c613b5c055b4
Aymen <izitouni917@gmail.com>
2025-04-09 00:18:38 +0100

    delete item by id + clear cart

--- back-end/routes/cartRoute.js
+++ back-end/routes/cartRoute.js
@@ -3,7 +3,8 @@ import getActiveCartForUser from '../services/cartService.js'
 import validateJWT from '../middlewares/validateJWT.js'
 import { addItemToCart } from '../services/cartService.js'
 import { updateCartInItem } from '../services/cartService.js'
-
+import { deleteItemIncart } from '../services/cartService.js'
+import { clearCart } from '../services/cartService.js'
 const router = express.Router()
 
 router.get('/',validateJWT,async (req,res)=>{
@@ -22,6 +23,20 @@ router.put('/items',validateJWT,async(req,res)=>{
     const userId = req.user._id
     const{productId,quantity} = req.body
     const response = await updateCartInItem({userId,productId,quantity})
+    res.status(response.statusCode).send(response.message)
+})
+
+// delete product by id
+router.delete('/items/:productId',validateJWT,async(req,res)=>{
+    const userId = req.user._id
+    const {productId} = req.params
+    const response = await deleteItemIncart({productId,userId})
+    res.status(response.statusCode).send(response.message)
+})
+// clear cart
+router.delete("/",validateJWT,async(req,res)=>{
+    const userId = req.user._id
+    const response = await clearCart({userId})
     res.status(response.statusCode).send(response.message)
 })
 export default router
